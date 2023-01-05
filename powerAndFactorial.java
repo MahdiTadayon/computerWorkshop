e7f51b1346b8b873e297111ac94964801467f65e
MahdiTadayon <mahditadayon789@yahoo.com>
2023-01-05 21:15:51 +0330

    Update and rename power&factorial.java to powerAndFactorial.java

--- powerAndFactorial.java
+++ powerAndFactorial.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class kargah_computer_10 {
+public class powerAndFactorial {
 	public static void main(String[] args ){
 		Scanner input = new Scanner(System.in);
 		int x = input.nextInt();
