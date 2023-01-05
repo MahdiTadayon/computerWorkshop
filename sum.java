9e4f446402491507548de26aec1afffb865f2082
MahdiTadayon <mahditadayon789@yahoo.com>
2023-01-05 21:18:08 +0330

    rename class

--- sum.java
+++ sum.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class kargah_computer_7 {
+public class sum {
 	public static void main(String[] args ){
 		Scanner input = new Scanner(System.in);
 		int n =input.nextInt();
