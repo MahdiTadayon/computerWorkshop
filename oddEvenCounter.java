a44004ee181594cb3632e33184a2a221283d055a
MahdiTadayon <mahditadayon789@yahoo.com>
2023-01-05 21:14:55 +0330

    rename class

--- oddEvenCounter.java
+++ oddEvenCounter.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class kargah_computer_18 {
+public class oddEvenCounter {
 	public static void main(String[] args ){
 		Scanner input = new Scanner(System.in);
 		
