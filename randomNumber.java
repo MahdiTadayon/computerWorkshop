a978037cf6f867195671636aa9815b4f67d757aa
MahdiTadayon <mahditadayon789@yahoo.com>
2023-01-05 21:17:07 +0330

    rename class

--- randomNumber.java
+++ randomNumber.java
@@ -1,6 +1,6 @@
 import java.lang.Math; 
 
-public class kargah_computer_8 {
+public class randomNumber {
 	public static void main(String[] args ){
 		for(int i=1;i<6;i++){
 			int random_number =(int)(Math.random() * (100-1)) + 1;
